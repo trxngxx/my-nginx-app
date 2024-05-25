72dd328c998ece271c2d0d2ecd3c70073a47b478
trxngxx <nnartnav@gmail.com>
2024-05-25 15:18:37 +0000

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Middleware
 app.use(helmet());
